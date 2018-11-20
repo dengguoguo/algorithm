c2291bfd791b0efad375afd3974b54275555a956
dengguo <609085005@qq.com>
2018-11-20 14:47:47 +0800

    布隆过滤器的实现

---
+++ bloomfilter.py
@@ -0,0 +1,85 @@
+# -*- coding: utf-8 -*-
+# @Time    : 2018/8/17 11:42
+# @Author  : dengguo
+# @File    : bloomfilter.py
+# @Software: PyCharm
+"""
+如果想判断一个元素是不是在一个集合里，一般想到的是将所有元素保存起来，然后通过比较确定。
+链表，树等等数据结构都是这种思路. 但是随着集合中元素的增加，我们需要的存储空间越来越大，检索速度也越来越慢。
+不过世界上还有一种叫作散列表（又叫哈希表，Hash table）的数据结构。
+它可以通过一个Hash函数将一个元素映射成一个位阵列（Bit Array）中的一个点。
+这样一来，我们只要看看这个点是不是 1 就知道可以集合中有没有它了。这就是布隆过滤器的基本思想。
+Hash面临的问题就是冲突。假设 Hash 函数是良好的，如果我们的位阵列长度为 m 个点，那么如果我们想将冲突率降低到例如 1%, 这个散列表就只能容纳 m/100 个元素。显然这就不叫空间有效了（Space-efficient）。
+解决方法也简单，就是使用多个 Hash，如果它们有一个说元素不在集合中，那肯定就不在。
+如果它们都说在，虽然也有一定可能性它们在说谎，不过直觉上判断这种事情的概率是比较低的。
+"""
+
+from bitarray import bitarray
+import mmh3
+
+
+class BloomFilter(set):
+
+    def __init__(self, size, hash_count):
+        super(BloomFilter, self).__init__()
+        self.bit_array = bitarray(size)
+        self.bit_array.setall(0)
+        self.size = size
+        self.hash_count = hash_count
+
+    def __len__(self):
+        return self.size
+
+    def __iter__(self):
+        return iter(self.bit_array)
+
+    def add(self, item):
+        for ii in range(self.hash_count):
+            index = mmh3.hash(item, ii) % self.size
+            self.bit_array[index] = 1
+
+        return self
+
+    def __contains__(self, item):
+        out = True
+        for ii in range(self.hash_count):
+            index = mmh3.hash(item, ii) % self.size
+            if self.bit_array[index] == 0:
+                out = False
+
+        return out
+
+
+def main():
+    bloom = BloomFilter(100, 10)
+    animals = ['dog', 'cat', 'giraffe', 'fly', 'mosquito', 'horse', 'eagle',
+               'bird', 'bison', 'boar', 'butterfly', 'ant', 'anaconda', 'bear',
+               'chicken', 'dolphin', 'donkey', 'crow', 'crocodile']
+    # First insertion of animals into the bloom filter
+    for animal in animals:
+        bloom.add(animal)
+
+    # Membership existence for already inserted animals
+    # There should not be any false negatives
+    for animal in animals:
+        if animal in bloom:
+            print('{} is in bloom filter as expected'.format(animal))
+        else:
+            print('Something is terribly went wrong for {}'.format(animal))
+            print('FALSE NEGATIVE!')
+
+    # Membership existence for not inserted animals
+    # There could be false positives
+    other_animals = ['badger', 'cow', 'pig', 'sheep', 'bee', 'wolf', 'fox',
+                     'whale', 'shark', 'fish', 'turkey', 'duck', 'dove',
+                     'deer', 'elephant', 'frog', 'falcon', 'goat', 'gorilla',
+                     'hawk']
+    for other_animal in other_animals:
+        if other_animal in bloom:
+            print('{} is not in the bloom, but a false positive'.format(other_animal))
+        else:
+            print('{} is not in the bloom filter as expected'.format(other_animal))
+
+
+if __name__ == '__main__':
+    main()
